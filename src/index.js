d850a713644271c9f10da710b8d667de0b15fdb8
Carefree-Carmander <69743984+Carefree-Carmander@users.noreply.github.com>
2020-12-28 21:45:24 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import Search from "./Search";
-import axios from "axios";
-import ReactAnimatedWeather from 'react-animated-weather';
 
 
 ReactDOM.render(
